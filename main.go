8148c1056af7b052943534037fcb57d9cb8c78b9
shyamauc <shyamauc@yahoo-corp.jp>
2017-02-16 18:42:42 +0900

    update

--- main.go
+++ main.go
@@ -25,19 +25,31 @@ func main() {
 
 	//Parse HTML By goquery module
 	prefPage.Find(".modal__inner .link-list li.link a").Each(func(_ int, train *goquery.Selection) {
+
 		//train.Attr("href")の例：https://tabelog.com/tokyo/R9/rstLst/
 		trainUrl, _ := train.Attr("href")
 		trainHtml, _ := goquery.NewDocument(trainUrl)
 
 		trainHtml.Find("li.station label a").Each(func(_ int, station *goquery.Selection) {
+
 			stationName := station.Text()
 			stationUrl, _ := station.Attr("href")
 
 			re := regexp.MustCompile("([a-z]*/A[0-9]*/A[0-9]*/R[0-9]*)")
 			stationId := re.FindStringSubmatch(stationUrl)
 
-			fmt.Println(stationName)
-			fmt.Println(stationId[1])
+			//MySQL
+			db, err := sql.Open("mysql", "b251699974322a:ff525dcc@tcp(us-cdbr-iron-east-04.cleardb.net:3306)/heroku_cf0097170d0f549")
+			if err != nil {
+				log.Fatal("open err: %v", err)
+			}
+			defer db.Close()
+
+			query := "INSERT INGORE INTO station (name, stationId) VALUES (?, ?)"
+			_, errInsert := db.Exec(query, stationName, stationId[1])
+			if errInsert != nil {
+				log.Fatal("insert error: ", errInsert)
+			}
 		})
 	})
 }
